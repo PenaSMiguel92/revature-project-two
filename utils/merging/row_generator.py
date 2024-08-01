ed8a3ee8fb817929337a17301c9f3f73902bf1f1
Miguel Pena <miguel674@revature.net>
2024-08-01 12:21:40 -0700

    implemented csv saver, and row generator, not done

--- utils/merging/row_generator.py
+++ utils/merging/row_generator.py
@@ -1,8 +1,18 @@
 
 
-def row_generator(*args, **kwargs) -> str:
+def row_generator(cur_row: int, customer: dict, transaction: dict, product: dict, **kwargs) -> str:
     """
         This method takes in dictionaries and generates a row to be written to csv file.
         
         Set errorize to True: errorize=True to randomly add in an error to the row.
+        ;param; cur_row: int, customer: dict, transaction: dict, product: dict, **kwargs
+        ;returns; str <- string combining columns
     """
+    result_str = ''
+    result_str += f'{cur_row},{customer.get('id'),{customer.get('full_name')}},{product.get('product_id')},'
+    result_str += f'{product.get('name')},{product.get('category')},{transaction.get('payment_type')},'
+    result_str += f'{product.get('quantity')},{product.get('price')},{transaction.get('datetime')},'
+    result_str += f'{customer.get('country')},{customer.get('city')},{product.get('website')},'
+    result_str += f'{transaction.get('payment_tx_id'),{transaction.get('payment_tx_success')},{transaction.get('failure_reason')}}'
+    return result_str
+
---
+++ utils/merging/save_csvfile.py
@@ -0,0 +1,17 @@
+import csv
+
+def save_data(rows_to_write: list[list[str]]) -> bool:
+    """
+        This function takes in a list of list of strings.
+
+        csv.writer.writerows joins each list of strings as comma seperated values.
+
+        This function returns a boolean when saving data to .csv file is complete
+    """
+    filename = 'ouput.csv'
+    with open(filename, 'w+', newline='') as output_file: 
+        #note that newline='' is for windows. since it does something like \n\n
+        csv_writer = csv.writer(output_file)
+        csv_writer.writerows(rows_to_write)
+
+    return True
