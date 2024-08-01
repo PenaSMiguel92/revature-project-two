516cd0e48c6e674c893efc9d6b4774e98de43bbd
Miguel Pena <miguel674@revature.net>
2024-08-01 11:22:49 -0700

    began writing merging tools

---
+++ utils/merging/row_generator.py
@@ -0,0 +1,8 @@
+
+
+def row_generator(*args, **kwargs) -> str:
+    """
+        This method takes in dictionaries and generates a row to be written to csv file.
+        
+        Set errorize to True: errorize=True to randomly add in an error to the row.
+    """
