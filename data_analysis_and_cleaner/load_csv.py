224c8e74447f4bdbc075d514655a9b857635011d
miguelpena-bigdata-dev <miguel674@revature.net>
2024-08-05 14:30:56 -0700

    still trying to fix

--- data_analysis_and_cleaner/load_csv.py
+++ data_analysis_and_cleaner/load_csv.py
@@ -1,7 +1,10 @@
 from pathlib import Path
+from pyspark import SparkContext, SparkConf
 import sparkContext
 def load():
     sc = sparkContext.sc
-    path = Path(__file__).parent / "../data/data_team_3.csv"
-    data_team_3_rdd = sc.textFile(path.as_uri)
+    path = Path(__file__).parent / "data/data_team_3.csv"
+    path.resolve()
+    print(path.absolute().root)
+    data_team_3_rdd = sc.textFile('file:///home/miguel674-rev/2009_Big_Data/revature-2/Revature_Project_2/data/data_team_3.py')
     return data_team_3_rdd.map(lambda x: x.split(','))
