941f0a9727309f05f85480151b1456d189e9ca20
matthewbernhardt23 <matt.bernhardt@yahoo.com>
2024-08-05 15:42:49 -0600

    cleaned data

--- data_analysis_and_cleaner/clean_data.py
+++ data_analysis_and_cleaner/clean_data.py
@@ -1,9 +1,52 @@
-import load_csv
-from pyspark import SparkContext, SparkConf
-import sparkContext
+import findspark
+findspark.init()
 
-sc = sparkContext.sc
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, countDistinct
 
-rdd_list = load_csv.load() 
+# Create a Spark session
+spark = SparkSession.builder \
+    .appName("JupyterPySpark") \
+    .getOrCreate()
 
-print(rdd_list.take(5))
+# Access the Spark context from the Spark session
+sc = spark.sparkContext
+
+# Verify the Spark session and context
+print(spark)
+print(sc)
+
+# Have to change the path to the location of the data file
+df = spark.read.format("csv").option("header", "true").load("/Users/matthewbernhardt/Desktop/data_team_3.csv")
+
+
+# Filter out rows where 'payment_txn_success' is 'Y'
+df = df.filter(df["payment_txn_success"] == 'Y')
+
+#  Drop Failure Reason Column
+df = df.drop("failure reason")
+
+
+# Define a list of columns to check for 'CORRUPTED'
+columns_to_check = df.columns
+print(columns_to_check)
+
+# Create a filter to exclude rows where any column value is 'CORRUPTED'
+for column in columns_to_check[1:]:
+    df = df.filter((col(column).isNotNull()) & (col(column) != "CORRUPTED"))
+
+# Apply the filter to the DataFrame
+df.count()
+
+
+def unique_values_count(df):
+    unique_counts = {}
+    for column in df.columns:
+        unique_count = df.select(countDistinct(column)).collect()[0][0]
+        unique_counts[column] = unique_count
+    return unique_counts
+
+print(unique_values_count(df))
+
+# Output the Cleaned DataFrame as a CSV
+df.write.csv("cleaned_data", header=True)
