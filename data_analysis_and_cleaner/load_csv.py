2b5acc243ec1c1c9be03abfd58154edc5d41a34f
miguelpena-bigdata-dev <miguel674@revature.net>
2024-08-05 14:43:11 -0700

    getting there

--- data_analysis_and_cleaner/load_csv.py
+++ data_analysis_and_cleaner/load_csv.py
@@ -4,7 +4,5 @@ import sparkContext
 def load():
     sc = sparkContext.sc
     path = Path(__file__).parent / "data/data_team_3.csv"
-    path.resolve()
-    print(path.absolute().root)
-    data_team_3_rdd = sc.textFile('file:///home/miguel674-rev/2009_Big_Data/revature-2/Revature_Project_2/data/data_team_3.py')
+    data_team_3_rdd = sc.textFile('File://'+str(path.absolute().as_posix))
     return data_team_3_rdd.map(lambda x: x.split(','))
