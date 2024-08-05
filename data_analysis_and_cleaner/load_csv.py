538c2d7bdd2de3dea8c426c73ec0a5ddad416fec
miguelpena-bigdata-dev <miguel674@revature.net>
2024-08-05 14:47:24 -0700

    relative file path fixed

--- data_analysis_and_cleaner/load_csv.py
+++ data_analysis_and_cleaner/load_csv.py
@@ -4,5 +4,5 @@ import sparkContext
 def load():
     sc = sparkContext.sc
     path = Path(__file__).parent / "data/data_team_3.csv"
-    data_team_3_rdd = sc.textFile('File://'+str(path.absolute().as_posix))
+    data_team_3_rdd = sc.textFile('file://'+str(path.absolute()))
     return data_team_3_rdd.map(lambda x: x.split(','))
