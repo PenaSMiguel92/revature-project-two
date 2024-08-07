f79477ffd82f45256094c3c75d98a82ad25070f3
miguelpena-bigdata-dev <miguel674@revature.net>
2024-08-07 12:07:18 -0700

    context for sql

--- data_analysis_and_cleaner/notebooks/load_csv.py
+++ data_analysis_and_cleaner/notebooks/load_csv.py
@@ -7,3 +7,9 @@ def load(path: str):
     #used in linux distro
     path = Path(__file__).parent / path
     return spark.read.format("csv").option("header", "true").load('file://'+str(path.absolute())) 
+
+def getContext():
+    """
+        Use this to get the current spark context
+    """
+    return sparkContext.spark
